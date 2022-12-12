45a5c2971057dbeebdd0164e992f7751e11f7461
austrisD <austris.daugulis@gmail.com>
2022-12-12 16:43:53 +0200

    inventory protection

--- kods.js
+++ kods.js
@@ -31,60 +31,80 @@ let laminatdeeli = [
 function veidotPlaanu() {
   let prefillCount = 200;
 
-  while (prefillCount > 0) {
+  while (0 < prefillCount) {
     let selectRandomPlank = Math.floor(Math.random() * laminatdeeli.length);
     let ROW = Math.floor(Math.random() * lamiPlan.length);
     let SPOT = Math.floor(Math.random() * 12);
     let notDisabledLocation = ROW < 9 && SPOT < 4 && !lamiPlan[ROW][SPOT] == "";
-    let notDisabledLocationNext = ROW > 9 && !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
+    let notDisabledLocationNext = !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
     let isPairNumberLocation = (ROW % 2 === 0 && SPOT % 2 == 0) || (ROW % 2 !== 0 && SPOT % 2 !== 0);
-    let attemptsCount = 50;
+    let inventoryEmpty = false;
+    let attemptsCount = 500;
     
 
     function newNumbers() {
       ROW = Math.floor(Math.random() * lamiPlan.length);
       SPOT = Math.floor(Math.random() * 12);
       notDisabledLocation = ROW < 9 && SPOT < 4 && !lamiPlan[ROW][SPOT] == "";
-      notDisabledLocationNext = ROW > 9 && !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
+      notDisabledLocationNext = !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
       isPairNumberLocation = (ROW % 2 === 0 && SPOT % 2 == 0) || (ROW % 2 !== 0 && SPOT % 2 !== 0);
     }
 
     //lokācijas pārbaude, ja vieta aizpildīta paņem citu  aa
-    while (attemptsCount > 0 > 0 && notDisabledLocation) {
+    while (attemptsCount > 0  && notDisabledLocation) {
       //make new numbers
       newNumbers();
       attemptsCount--;
     }
 
-    if (attemptsCount < 0) console.log("mēģinājumu skaits izmelts");
+    if (attemptsCount > 0) console.log("mēģinājumu skaits izmelts");
 
     ////
-    console.log(isPairNumberLocation + " :" + ROW + " :" + SPOT);
+    // console.log(isPairNumberLocation + " :" + ROW + " :" + SPOT);
     ////
     //ja nav vairs dēli paņemt citu
-    while (laminatdeeli[selectRandomPlank].count <= 1) {
+    while (
+      attemptsCount > 0 &&
+      laminatdeeli[selectRandomPlank].count <= 0 
+    ) {
       selectRandomPlank = Math.floor(Math.random() * laminatdeeli.length);
+      attemptsCount--;
     }
-    //viss labi, var ievietot dēli
-    if (attemptsCount > 0 && isPairNumberLocation) {
-      lamiPlan[ROW][SPOT] = laminatdeeli[selectRandomPlank].key;
 
-      const nextRow = ROW + 1;
-      console.log(notDisabledLocationNext);
-      console.log((SPOT + 1).valueOf());
-      //ievietot deli nakosa pozīcijaa 
+
+
+    //viss labi, var ievietot dēli
+    if (isPairNumberLocation || !inventoryEmpty) {
+      lamiPlan[ROW][SPOT] = laminatdeeli[selectRandomPlank].key;
+      laminatdeeli[selectRandomPlank].count =
+        laminatdeeli[selectRandomPlank].count - 1; //noņet dēli no inventorija
+
+      //ievietot deli nakosa pozīcijaa
       if (notDisabledLocationNext) {
         lamiPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
         if ((ROW + 1).valueOf() < 9 && (SPOT + 1).valueOf() > 4) {
-          console.log("deelu ievietošana else if");
           lamiPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
         }
-        if ((SPOT + 1).valueOf() > 12 && (ROW + 1).valueOf() < lamiPlan.length &&  (ROW + 1).valueOf() > 9 ) {
-          lamiPlan[ROW+1][0] = laminatdeeli[selectRandomPlank].key;
+        if (
+          (SPOT + 1).valueOf() > 12 &&
+          (ROW + 1).valueOf() < lamiPlan.length &&
+          (ROW + 1).valueOf() > 9
+        ) {
+          lamiPlan[ROW + 1][0] = laminatdeeli[selectRandomPlank].key;
         }
-      } 
-      laminatdeeli[selectRandomPlank].count =
-        laminatdeeli[selectRandomPlank].count - 1; //noņet dēli no inventorija
+      }
+    }
+   
+
+    if (
+      laminatdeeli[0].count < 1 &&
+      laminatdeeli[1].count < 1 &&
+      laminatdeeli[2].count < 1 &&
+      laminatdeeli[3].count < 1 &&
+      laminatdeeli[4].count < 1 ) {
+      prefillCount = 0 
+      inventoryEmpty = true;
+      console.log("inventorijs ir tukš");
     }
 
     prefillCount--;
