793ef5a4af8e3593816d5dc50d6f5a9a54fa6584
austrisD <austris.daugulis@gmail.com>
2022-12-12 17:33:44 +0200

    update

--- kods.js
+++ kods.js
@@ -1,24 +1,20 @@
-let lamiPlan = [
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-  [[""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""], [""]],
-];
+let FloorPlan = [];
+for(let i = 0; i < 20; i++) {
+  FloorPlan.push([
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+    ["__________"],
+  ]);
+}
 
 let laminatdeeli = [
   { count: 10, key: "darkSimple" },
@@ -33,21 +29,21 @@ function veidotPlaanu() {
 
   while (0 < prefillCount) {
     let selectRandomPlank = Math.floor(Math.random() * laminatdeeli.length);
-    let ROW = Math.floor(Math.random() * lamiPlan.length);
+    let ROW = Math.floor(Math.random() * FloorPlan.length);
     let SPOT = Math.floor(Math.random() * 12);
-    let notDisabledLocation = ROW < 9 && SPOT < 4 && !lamiPlan[ROW][SPOT] == "";
-    let notDisabledLocationNext = !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
+    let notDisabledLocation = ROW < 9 && SPOT < 4;
+    let notDisabledLocationNext = FloorPlan[ROW][(SPOT+1).valueOf()] == "__________";
     let isPairNumberLocation = (ROW % 2 === 0 && SPOT % 2 == 0) || (ROW % 2 !== 0 && SPOT % 2 !== 0);
     let inventoryEmpty = false;
     let attemptsCount = 500;
     
 
     function newNumbers() {
-      ROW = Math.floor(Math.random() * lamiPlan.length);
+      ROW = Math.floor(Math.random() * FloorPlan.length);
       SPOT = Math.floor(Math.random() * 12);
-      notDisabledLocation = ROW < 9 && SPOT < 4 && !lamiPlan[ROW][SPOT] == "";
-      notDisabledLocationNext = !lamiPlan[ROW][(SPOT+1).valueOf()] == "";
-      isPairNumberLocation = (ROW % 2 === 0 && SPOT % 2 == 0) || (ROW % 2 !== 0 && SPOT % 2 !== 0);
+      notDisabledLocation = ROW < 9 && SPOT < 4;
+      notDisabledLocationNext = !FloorPlan[ROW][(SPOT+1).valueOf()] == "__________";
+      isPairNumberLocation = (ROW % 2 == 0 && SPOT % 2 == 0) || (ROW % 2 !== 0 && SPOT % 2 !== 0);
     }
 
     //lokācijas pārbaude, ja vieta aizpildīta paņem citu  aa
@@ -75,23 +71,25 @@ function veidotPlaanu() {
 
     //viss labi, var ievietot dēli
     if (isPairNumberLocation || !inventoryEmpty) {
-      lamiPlan[ROW][SPOT] = laminatdeeli[selectRandomPlank].key;
-      laminatdeeli[selectRandomPlank].count =
-        laminatdeeli[selectRandomPlank].count - 1; //noņet dēli no inventorija
+      FloorPlan[ROW][SPOT] = laminatdeeli[selectRandomPlank].key;
+      laminatdeeli[selectRandomPlank].count = laminatdeeli[selectRandomPlank].count - 1; //noņet dēli no inventorija
 
       //ievietot deli nakosa pozīcijaa
       if (notDisabledLocationNext) {
-        lamiPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
+        FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
         if ((ROW + 1).valueOf() < 9 && (SPOT + 1).valueOf() > 4) {
-          lamiPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
+          FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
         }
-        if (
-          (SPOT + 1).valueOf() > 12 &&
-          (ROW + 1).valueOf() < lamiPlan.length &&
-          (ROW + 1).valueOf() > 9
-        ) {
-          lamiPlan[ROW + 1][0] = laminatdeeli[selectRandomPlank].key;
-        }
+
+
+        // if (
+        //   ROW + 1 < FloorPlan.length &&
+        //   ROW + 1 > 9
+        // ) {
+        //   FloorPlan[ROW + 1][0] = laminatdeeli[selectRandomPlank].key;
+        // }
+
+
       }
     }
    
@@ -107,6 +105,14 @@ function veidotPlaanu() {
       console.log("inventorijs ir tukš");
     }
 
+    // laminatdeeli.map((item) => {
+    //   if (item.count < 1) {
+    //     prefillCount = 0;
+    //     inventoryEmpty = true;
+    //     console.log(item.key, "inventorijs ir tukš");
+    //   }
+    // });
+
     prefillCount--;
     
   }
@@ -116,11 +122,11 @@ veidotPlaanu();
 
 // let vienaadoSkaits = 0;
 
-// for (index = 0; index < lamiPlan.length; index++) {
-//   for (rowNr = 0; rowNr < lamiPlan[rowNr].length; rowNr++) {
+// for (index = 0; index < FloorPlan.length; index++) {
+//   for (rowNr = 0; rowNr < FloorPlan[rowNr].length; rowNr++) {
 //     let attemtCount = 0;
 //     while (
-//       lamiPlan[index][rowNr - 1] == lamiPlan[index][rowNr] &&
+//       FloorPlan[index][rowNr - 1] == FloorPlan[index][rowNr] &&
 //       attemtCount < 100
 //     ) {
 //       attemtCount = attemtCount + 1;
@@ -131,6 +137,6 @@ veidotPlaanu();
 // }
 
 console.log("====================================");
-console.log(lamiPlan);
+console.log(FloorPlan);
 console.log(laminatdeeli);
 console.log("====================================");
