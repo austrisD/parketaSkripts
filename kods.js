615262c50033821bc2cbd3197023ce5f87a1af0b
austrisD <austris.daugulis@gmail.com>
2022-12-13 13:59:57 +0200

    color displaying

--- kods.js
+++ kods.js
@@ -1,27 +1,27 @@
 let FloorPlan = [];
 for(let i = 0; i < 20; i++) {
   FloorPlan.push([
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
-    ["_disabled_"],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
+    ['_disabled_'],
   ]);
 }
 
 let laminatdeeli = [
-  { count: 10, key: "darkSimple" },
-  { count: 19, key: "___lite___" },
-  { count: 26, key: "___dark___" },
-  { count: 27, key: "_darkLite_" },
-  { count: 33, key: "___main___" },
+  { count: 10, key: "darkSimple" }, //darkSimple
+  { count: 19, key: "___lite___" }, //___lite___
+  { count: 26, key: "___dark___" }, //___dark___
+  { count: 27, key: "_darkLite_" }, //_darkLite_
+  { count: 33, key: "___main___" }, //___main___
 ];
 
 function veidotPlaanu() {
@@ -32,20 +32,23 @@ function veidotPlaanu() {
     let ROW = Math.floor(Math.random() * FloorPlan.length);
     let SPOT = Math.floor(Math.random() * 12);
     let notDisabledLocation = ROW < 9 && SPOT < 4;
-    let notDisabledLocationNext = FloorPlan[ROW][(SPOT+1).valueOf()] == "_disabled_";
-    let isPairNumberLocation = ROW % 2 == 0 && SPOT % 2 == 0 || ROW % 2 !== 0 && SPOT % 2 !== 0;
+    let notDisabledLocationNext = FloorPlan[ROW][SPOT+1] == "_disabled_";
+    let isPairNumberLocation = ROW % 2 == 0 && SPOT % 2 == 0;
     let inventoryEmpty = false;
     let attemptsCount = 500;
 
 
-    
+
+    console.log('[paara rinda'+ ROW);
+    console.log(ROW % 2 == 0  );
+
 
     function newNumbers() {
       ROW = Math.floor(Math.random() * FloorPlan.length);
       SPOT = Math.floor(Math.random() * 12);
       notDisabledLocation = ROW < 9 && SPOT < 4;
-      notDisabledLocationNext = FloorPlan[ROW][(SPOT+1).valueOf()] == "_disabled_";
-      isPairNumberLocation = ROW % 2 == 0 && SPOT % 2 == 0 || ROW % 2 !== 0 && SPOT % 2 !== 0;
+      notDisabledLocationNext = FloorPlan[ROW][SPOT+1] == "_disabled_";
+      isPairNumberLocation = ROW % 2 == 0 && SPOT % 2 == 0 ;
     }
 
     //lokācijas pārbaude, ja vieta aizpildīta paņem citu  aa
@@ -75,22 +78,24 @@ function veidotPlaanu() {
       FloorPlan[ROW][SPOT] = laminatdeeli[selectRandomPlank].key;
       laminatdeeli[selectRandomPlank].count = laminatdeeli[selectRandomPlank].count - 1; //noņet dēli no inventorija
 
+         
+
+
       //ievietot deli nakosa pozīcijaa
 
+      // if (notDisabledLocationNext && SPOT + 1 == "_disabled_") {
+      //   FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
+      //   if ((ROW + 1).valueOf() < 9 && (SPOT + 1).valueOf() > 4) {
+      //     FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
+      //   }
 
-      if (notDisabledLocationNext && SPOT + 1 == "_disabled_") {
-        FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
-        if ((ROW + 1).valueOf() < 9 && (SPOT + 1).valueOf() > 4) {
-          FloorPlan[ROW][SPOT + 1] = laminatdeeli[selectRandomPlank].key;
-        }
-
-        // if (
-        //   ROW + 1 < FloorPlan.length &&
-        //   ROW + 1 > 9
-        // ) {
-        //   FloorPlan[ROW + 1][0] = laminatdeeli[selectRandomPlank].key;
-        // }
-      }
+      //   if (
+      //     ROW + 1 < FloorPlan.length &&
+      //     ROW + 1 > 9
+      //   ) {
+      //     FloorPlan[ROW + 1][0] = laminatdeeli[selectRandomPlank].key;
+      //   }
+      // }
     }
    
 
